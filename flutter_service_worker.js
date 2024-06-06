a9a8c1eaa7901e1643e0cc0402b3d40f63831954
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2024-06-06 23:34:50 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3660ca5628bffecc156e183a5841e7b8",
-"index.html": "acc8ec7e6ec9db3b3361982cef7da541",
-"/": "acc8ec7e6ec9db3b3361982cef7da541",
-"main.dart.js": "66b7dd97aec81ad2c3d1b477f302f33f",
+"index.html": "d487c57fd70272e07bc0d3e2be50b6a7",
+"/": "d487c57fd70272e07bc0d3e2be50b6a7",
+"main.dart.js": "6b31a5f7a0bf1a917e9ee59a2655e00f",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "7b3bc6c3edc2ade488de2aa2e4e6bff4",
 "icons/Icon-192.png": "6122a562b8299e150b6d79ca6660b156",
